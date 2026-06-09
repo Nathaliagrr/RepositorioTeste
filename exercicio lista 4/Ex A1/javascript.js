4c8713a3f2d810defc3c04f70fcb37cf4e76208d
NATHÁLIA LETICIA RODRIGUES <nathalia_rodrigue150@estudante.sesisenai.org.br>
2026-06-09 20:59:42 -0300

    Resolução do ex A1

---
+++ exercicio lista 4/Ex A1/javascript.js
@@ -0,0 +1,8 @@
+const titulo = document.querySelector("#titulo");
+console.log(titulo.textContent);
+
+const descricao = document.querySelector(".descricao");
+console.log(descricao.textContent);
+
+const button = document.querySelector("#btn");
+console.log(button.tagName);
