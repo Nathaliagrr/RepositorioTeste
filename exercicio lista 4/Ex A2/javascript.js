f745a95beefefb0e300a5688d119a01c7727336d
NATHÁLIA LETICIA RODRIGUES <nathalia_rodrigue150@estudante.sesisenai.org.br>
2026-06-09 21:18:43 -0300

    Exercicios A2

---
+++ exercicio lista 4/Ex A2/javascript.js
@@ -0,0 +1,7 @@
+const titulo = document.querySelector("#titulo");
+console.log(titulo.textContent = "JavaScript no DOM");
+
+const subtitulo = document.querySelector("#subtitulo");
+console.log(subtitulo.textContent = "Nathália Letícia Rodrigues");
+
+caixa.innerHTML = "<strong>Conteúdo alterado</strong>"
